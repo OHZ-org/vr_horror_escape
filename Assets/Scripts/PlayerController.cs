1eab4b487520dfb63880db541dcb07d1c07bbbe5
VALHALL4 <hsm011007@naver.com>
2023-07-07 15:16:23 +0900

    추가

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : player_base
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateState();
+    }
+
+    protected override void UpdateIdle()
+    {
+
+    }
+
+    protected override void UpdateMove()
+    {
         
+    }
+
+    protected override void UpdateInteraction()
+    {
+
     }
 }
