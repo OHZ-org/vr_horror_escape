ea53d967a6bb76cc9d0640ecf85f4d796ad46be6
100jnghn <gnswhd0415@gmail.com>
2023-07-07 15:29:26 +0900

    P1 Animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class PlayerController : player_base
 {
+
+    float speed = 5f;
+
+    Rigidbody rigidbody;
+    Vector3 movement;
+    float h, v;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -23,11 +31,20 @@ public class PlayerController : player_base
 
     protected override void UpdateMove()
     {
-        
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
     }
 
     protected override void UpdateInteraction()
     {
 
     }
+}
+
+
+void FixedUpdate()
+{
+    movement.Set(h, 0f, v);
+    movement = movement.normalized * speed * Time.deltaTime;
+    rigidbody.MovePosition(transform.position + movement);
 }
