d58d2d5326f818e31e04872022d60f41740c3ecc
VALHALL4 <hsm011007@naver.com>
2023-07-14 01:57:42 +0900

    문제 ui

--- Assets/Scripts/object/ProblemUI.cs
+++ Assets/Scripts/object/ProblemUI.cs
@@ -6,9 +6,19 @@ using UnityEngine.UI;
 
 public class ProblemUI : MonoBehaviour
 {   [SerializeField]
-    private GameObject popub;
-    private Image pbimage;
+    private GameObject popub;//문제 나오는 ui 창
+    [SerializeField]
+    private Image pb_image;
+    public static ProblemUI instance;
     // Start is called before the first frame update
+
+    private void Awake()//싱글톤 패턴
+    {
+        if (ProblemUI.instance == null)
+        {
+            ProblemUI.instance = this;
+        }
+    }
     void Start()
     {
         popub.SetActive(false);
@@ -20,14 +30,21 @@ public class ProblemUI : MonoBehaviour
         
     }
     
-    public void ShowPopUp( sprite)
+    public void ShowPopUp(Sprite sprite)
     {
+        Debug.Log("팝업 생성");
         popub.SetActive(true);
-        pbimage = sprite;
+        setImage(sprite);
     }
 
-    public void ClosePopUp()
+    public void ClosePopUp()//문제 창 닫는 함수
     {
+        Debug.Log("버튼 누름");
         popub.SetActive(false);
+    }
+
+    private void setImage(Sprite sprite)
+    {
+        pb_image.sprite = sprite;
     }
 }
--- Assets/Scripts/object/object_base.cs
+++ Assets/Scripts/object/object_base.cs
@@ -6,25 +6,30 @@ using UnityEngine.UI;
 public class object_base : MonoBehaviour
 {
     private OVRGrabbable ovrGrabbable;
-    private ProblemUI pbUI;
-    public Sprite sprite;
+    //private ProblemUI pbUI;
+    public Sprite pb_sprite;//문제 이미지 객체
     // Start is called before the first frame update
     void Start()
     {
-        
+        showPb();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //    if (ovrGrabbable.isGrabbed) //
+        //    {
+        //        showPb();
+        //    }
+
     }
-    
+
     //잡기->isgrabbed true->팝업창 띄우기
 
-    public virtual void isGrabbed()
+    public void showPb()
     {
-        pbUI.ShowPopUp(sprite);
+        Debug.Log("문제 이미지 바뀜");
+        ProblemUI.instance.ShowPopUp(pb_sprite);//문제 이미지 바꿔주는 함수
     }
  
 }
