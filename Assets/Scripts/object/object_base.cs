e91b5dfe93a6a494ff7525dff19ad72fb8c4377c
VALHALL4 <hsm011007@naver.com>
2023-07-14 16:56:33 +0900

    문제 ui 수정

--- Assets/Scripts/object/object_base.cs
+++ Assets/Scripts/object/object_base.cs
@@ -11,16 +11,16 @@ public class object_base : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        showPb();
+        //showPb();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //    if (ovrGrabbable.isGrabbed) //
-        //    {
-        //        showPb();
-        //    }
+        if (ovrGrabbable.isGrabbed)
+        {
+            showPb();
+        }
 
     }
 
