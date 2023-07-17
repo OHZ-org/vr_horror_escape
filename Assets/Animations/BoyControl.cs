c28baf4f561daf0dd5cf7db7e7bc6bfbfcfe2f7e
sum130 <sm3904@naver.com>
2023-07-18 01:48:59 +0900

    1반 꾸미기, 2P설정 수정

--- Assets/Animations/BoyControl.cs
+++ Assets/Animations/BoyControl.cs
@@ -11,7 +11,7 @@ public class Control : MonoBehaviour
     public Transform PlayerTr;
     public Animator animator;
 
-    public float moveSpd = 5.0f;
+    public float moveSpd = 1.0f;
 
     // Start is called before the first frame update
     void Start()
