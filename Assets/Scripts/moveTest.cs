b4c8b870cff714a37812b014488107dcde1fef14
100jnghn <gnswhd0415@gmail.com>
2023-07-18 17:26:32 +0900

    이동이 됐으면..

--- Assets/Scripts/moveTest.cs
+++ Assets/Scripts/moveTest.cs
@@ -9,39 +9,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float moveSpeed = 3.0f;  // 이동 속도를 조절할 변수
 
-    public float v = 0.0f;
-    public float h = 0.0f;
-    public float moveSpeed = 0.00001f;
+    private Rigidbody rigidbody;
+    private Animator animator;
+    private Camera mainCamera;
 
-    public Transform PlayerTr;
-    public Animator animator;
-    public Rigidbody rigidbody;
-  
-
-    void Start()
+    private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
-        PlayerTr = GetComponent<Transform>();
         animator = GetComponent<Animator>();
+        mainCamera = Camera.main;
     }
-    void Update()
+
+    private void Update()
     {
-        h = Input.GetAxis("Horizontal");
-        v = Input.GetAxis("Vertical");
-        animator.SetFloat("v", v);
-       PlayerTr.Translate(new Vector3(h,0,v) * moveSpeed * Time.deltaTime);
-        
-        if(v!=0.0f)
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(h, 0, v);
+        movement.Normalize();  // 대각선 이동 시 이동 벡터를 정규화하여 일정한 속도로 이동하도록 함
+
+        Vector3 cameraForward = Vector3.Scale(mainCamera.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 moveDirection = cameraForward * v + mainCamera.transform.right * h;
+
+        rigidbody.MovePosition(transform.position + moveDirection * moveSpeed * Time.deltaTime);
+
+        if (movement != Vector3.zero)
         {
+            transform.rotation = Quaternion.LookRotation(moveDirection);  // 이동 방향을 향하도록 플레이어의 회전을 설정
             animator.SetBool("isWalking", true);
         }
         else
         {
             animator.SetBool("isWalking", false);
         }
-
-        
     }
-
 }
