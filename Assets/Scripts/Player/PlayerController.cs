24782ec7fcee0b3254273629010248d798200b4a
앙쌀라 <89465775+kimsuyeon0702@users.noreply.github.com>
2023-07-18 17:04:46 +0900

    애셋 정리

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : player_base
+{
+
+    float speed = 5f;
+
+    Rigidbody rigidbody;
+    Vector3 movement;
+    float h, v;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateState();
+    }
+
+    protected override void UpdateIdle()
+    {
+
+    }
+
+    protected override void UpdateMove()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+    }
+
+    protected override void UpdateInteraction()
+    {
+
+    }
+}
+
+
+//void FixedUpdate()
+//{
+//    movement.Set(h, 0f, v);
+//    movement = movement.normalized * speed * Time.deltaTime;
+//    rigidbody.MovePosition(transform.position + movement);
+//}
