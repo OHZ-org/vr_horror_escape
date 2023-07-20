b1f1a4f3c6c4c8838862c1c967c5ca428d028810
VALHALL4 <hsm011007@naver.com>
2023-07-20 21:25:15 +0900

    ui 수정

--- Assets/Scripts/object/object_base.cs
+++ Assets/Scripts/object/object_base.cs
@@ -5,13 +5,18 @@ using UnityEngine.UI;
 
 public class object_base : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject popub;//문제 나오는 ui 창
+    [SerializeField]
+    private Image pb_image;
+    
     private OVRGrabbable ovrGrabbable;
-    //private ProblemUI pbUI;
+
     public Sprite pb_sprite;//문제 이미지 객체
     // Start is called before the first frame update
     void Start()
     {
-       
+        ShowPopUp();
     }
 
     // Update is called once per frame
@@ -21,16 +26,29 @@ public class object_base : MonoBehaviour
         //{
         //    showPb();
         //}
-        showPb();
+       
 
     }
 
     //잡기->isgrabbed true->팝업창 띄우기
 
-    public void showPb()
+
+    public void ShowPopUp()
     {
-        Debug.Log("문제 이미지 바뀜");
-        ProblemUI.instance.ShowPopUp(pb_sprite);//문제 이미지 바꿔주는 함수
+        Debug.Log("팝업 생성");
+        popub.SetActive(true);
+        setImage(pb_sprite);
     }
- 
+
+    public void ClosePopUp()//문제 창 닫는 함수
+    {
+        Debug.Log("버튼 누름");
+        popub.SetActive(false);
+    }
+
+    private void setImage(Sprite sprite)
+    {
+        pb_image.sprite = sprite;
+    }
+
 }
