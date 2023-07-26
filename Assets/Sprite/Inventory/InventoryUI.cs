0d80ecb656669db40aeb646c7d738e4816a2c9f4
sum130 <sm3904@naver.com>
2023-07-26 15:23:27 +0900

    인벤토리 Y버튼

--- Assets/Sprite/Inventory/InventoryUI.cs
+++ Assets/Sprite/Inventory/InventoryUI.cs
@@ -3,9 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
 
 public class InventoryUI : MonoBehaviour
 {
+    private InputAction inventoryAction;
     Inventory inven;
     public GameObject inventoryPanel;
     bool activeInventory = false;
@@ -31,14 +34,27 @@ public class InventoryUI : MonoBehaviour
         }
     }
 
+    private void Awake()
+    {
+        inventoryAction = new InputAction(binding: "<Gamepad>/buttonSouth");
+    }
+    private void OnEnable()
+    {
+        inventoryAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inventoryAction.Disable();
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.I))
+        if (inventoryAction.triggered)
         {
             activeInventory = !activeInventory;
             inventoryPanel.SetActive(activeInventory);
         }
-        
     }
 
     public void AddSlot()
