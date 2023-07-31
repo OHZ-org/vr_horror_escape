c7dde0ec1c17d195cb3fc5466bd78b07548bcbe6
100jnghn <gnswhd0415@gmail.com>
2023-07-31 11:20:20 +0900

    애니메이션 드디어 됨

--- Assets/Animations/UseKeyNotice/Door/Door1/Door1.cs
+++ Assets/Animations/UseKeyNotice/Door/Door1/Door1.cs
@@ -1,28 +1,32 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Diagnostics;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Door1 : MonoBehaviour
 {
+
     KeyCollision door;
     Animator anim;
 
     void Start()
     {
-        door = FindObjectOfType<KeyCollision>();
-        anim = GetComponent<Animator>();
+         door = FindObjectOfType<KeyCollision>();
+         anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        
         if (!door.IsLocked())
         {
-            anim.SetBool("Open", true);
-            UnityEngine.Debug.Log("@@@@@@Animation Play.");
-            UnityEngine.Debug.Log(anim.GetBool("Open"));
+             anim.SetBool("Open", true);
+             //UnityEngine.Debug.Log("@@@@@@Animation Play.");
+             //UnityEngine.Debug.Log(anim.GetBool("Open"));
         }
+        
     }
 }
