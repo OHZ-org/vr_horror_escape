4bb344ec2fca7e632309d4695a835faf84a7bbf0
VALHALL4 <hsm011007@naver.com>
2023-08-03 11:55:20 +0900

    문제 정답 스크립트

--- Assets/Scripts/object/Pb_ox.cs
+++ Assets/Scripts/object/Pb_ox.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pb_ox : MonoBehaviour
 {
+    public InputField inputAnswer;
+    public GameObject key;
+    public GameObject pbbook;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,18 @@ public class Pb_ox : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void judgeAnswer()
+    {
+        if (inputAnswer.text == "dd")
+        {
+            Debug.Log("정답");
+            Instantiate(key, pbbook.transform);
+        }
+        else
+        {
+            Debug.Log("오답");
+        }
     }
 }
