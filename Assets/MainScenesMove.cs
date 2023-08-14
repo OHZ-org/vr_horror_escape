0e6796e7ca4b0f21feb1fc04a49022c827f8469c
sum130 <sm3904@naver.com>
2023-08-14 10:42:40 +0900

    StartScnen

---
+++ Assets/MainScenesMove.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainScenesMove : MonoBehaviour
+{
+    public void MainScenesCtrl()
+    {
+        SceneManager.LoadScene("Main");
+        Debug.Log("Main Scenes Go");
+    }
+
+}
